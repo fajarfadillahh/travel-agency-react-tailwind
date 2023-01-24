a16ae7f5baf99592e797709acc5d1020e7ca792b
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-24 20:18:25 +0700

    responsive Sponsor

--- src/components/Sponsor.jsx
+++ src/components/Sponsor.jsx
@@ -9,14 +9,14 @@ import SponsorImg5 from "../assets/images/sponsors5.png";
 
 export default function Sponsor() {
   return (
-    <section className="sponsor section">
+    <section className="sponsor py-12">
       <div className="sponsor__container container">
-        <div className="sponsor__group grid grid-cols-2 justify-items-center gap-x-6 gap-y-14">
+        <div className="sponsor__group grid grid-cols-2 justify-items-center gap-x-6 gap-y-16 xs:grid-cols-3 md:mx-auto md:max-w-lg md:gap-x-12 lg:max-w-2xl lg:grid-cols-5 xl:max-w-5xl">
           <div className="sponsor__content">
             <img
               src={SponsorImg1}
               alt="sponsor img"
-              className="sponsor__img w-24 invert-[0.7] transition-all duration-400 hover:invert-[0.4]"
+              className="sponsor__img w-24 invert-[0.7] transition-all duration-400 hover:invert-[0.4] xl:w-28"
             />
           </div>
 
@@ -24,7 +24,7 @@ export default function Sponsor() {
             <img
               src={SponsorImg2}
               alt="sponsor img"
-              className="sponsor__img w-24 invert-[0.7] transition-all duration-400 hover:invert-[0.4]"
+              className="sponsor__img w-24 invert-[0.7] transition-all duration-400 hover:invert-[0.4] xl:w-28"
             />
           </div>
 
@@ -32,7 +32,7 @@ export default function Sponsor() {
             <img
               src={SponsorImg3}
               alt="sponsor img"
-              className="sponsor__img w-24 invert-[0.7] transition-all duration-400 hover:invert-[0.4]"
+              className="sponsor__img w-24 invert-[0.7] transition-all duration-400 hover:invert-[0.4] xl:w-28"
             />
           </div>
 
@@ -40,7 +40,7 @@ export default function Sponsor() {
             <img
               src={SponsorImg4}
               alt="sponsor img"
-              className="sponsor__img w-24 invert-[0.7] transition-all duration-400 hover:invert-[0.4]"
+              className="sponsor__img w-24 invert-[0.7] transition-all duration-400 hover:invert-[0.4] xl:w-28"
             />
           </div>
 
@@ -48,7 +48,7 @@ export default function Sponsor() {
             <img
               src={SponsorImg5}
               alt="sponsor img"
-              className="sponsor__img w-24 invert-[0.7] transition-all duration-400 hover:invert-[0.4]"
+              className="sponsor__img w-24 invert-[0.7] transition-all duration-400 hover:invert-[0.4] xl:w-28"
             />
           </div>
         </div>
