f67804cb99fd213ed59ccda2a556b407df0a08d7
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-24 23:34:46 +0700

    dark mode Sponsor

--- src/components/Sponsor.jsx
+++ src/components/Sponsor.jsx
@@ -9,7 +9,7 @@ import SponsorImg5 from "../assets/images/sponsors5.png";
 
 export default function Sponsor() {
   return (
-    <section className="sponsor py-12">
+    <section className="sponsor py-12 transition-all duration-400 dark:bg-gray-900">
       <div className="sponsor__container container">
         <div className="sponsor__group grid grid-cols-2 justify-items-center gap-x-6 gap-y-16 xs:grid-cols-3 md:mx-auto md:max-w-lg md:gap-x-12 lg:max-w-2xl lg:grid-cols-5 xl:max-w-5xl">
           <div className="sponsor__content">
